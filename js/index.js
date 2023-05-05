eda605ae4fef6bf63645faa6a09bb4d41c7fb473
Mica <miicka.20@gmail.com>
2023-05-05 17:51:04 -0300

    clase JS

---
+++ js/index.js
@@ -0,0 +1,43 @@
+// Variables //
+
+
+let nombre, apellido, edad, mascotas
+
+nombre = 'Micaela'
+apellido = 'Pascual'
+edad = 31
+mascotas = 'Si'
+
+console.log(nombre)
+console.log(apellido)
+console.log(edad)
+console.log(mascotas)
+
+
+
+// OPERACIONES MATEMATICAS //
+
+let n1 = 16
+let n2 = 68
+let n3 = 32
+ 
+
+// Suma //
+
+let resultadoSuma = n1 + n3
+console.log(resultadoSuma)
+
+// Resta //
+
+let resultadoResta = n3 - n2
+console.log(resultadoResta)
+
+// Multiplicacion //
+ 
+let resultadoMult = n2 * n3
+console.log(resultadoMult)
+
+// Division //
+
+let resultadoDiv = n2 / n1
+console.log(resultadoDiv)
